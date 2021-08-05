d32600816e016683215af067c630e523f83351e2
León Jaramillo <leonjaramillo@gmail.com>
2021-08-05 11:01:29 -0500

    Adding class Calculadora, being tested.

---
+++ main/java/co/edu/udea/udea_ruta2_ciclo2/poo/pruebas/Calculadora.java
@@ -0,0 +1,12 @@
+package co.edu.udea.udea_ruta2_ciclo2.poo.pruebas;
+
+public class Calculadora {
+
+    public Double cuadrado(Double numero) {
+        return numero * numero;
+    }
+
+    public Double cubo(Double numero) {
+        return numero * numero * numero;
+    }
+}
